f0779c0b99a2504e7f209217710a175d0c404f8f
Ezeani Ikenna Evan <32799580+iykeevans@users.noreply.github.com>
2023-12-02 15:21:17 +0100

    fix child button not showing

---
+++ src/components/Emits/Parent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div></div>
+
+    <Child :handleChangeMessage="handleChangeMessage" />
+  </div>
+</template>
+
+<script>
+import Child from "./Child.vue";
+export default {
+  components: { Child },
+  data() {
+    return {};
+  },
+  methods: {
+    handleChangeMessage() {
+      this.message = "good afternoon";
+    },
+  },
+};
+</script>
+
+<style scoped></style>
